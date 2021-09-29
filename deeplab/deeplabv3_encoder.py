5cfb5d6f45f176717fc3cdafca45a4f352cafffc
Wenbo <39964574@qq.com>
2021-09-29 15:57:07 +1300

    Add get_param for encoders to return parameters for training

--- deeplab/deeplabv3_encoder.py
+++ deeplab/deeplabv3_encoder.py
@@ -6,7 +6,7 @@ import deeplab.config as config
 import deeplab.residual_net as rn
 
 # https://github.com/pytorch/vision/blob/master/torchvision/models/segmentation/deeplabv3.py
-
+# Parallel modules with atrous convolution (ASPP)
 class ASPP(nn.Module):
     def __init__(self, input_channels, output_channels, depth, dilation_series, padding_series):
         super(ASPP, self).__init__()
@@ -85,10 +85,11 @@ class ASPP(nn.Module):
         
         return out
 
-
+"""
+RGBEncoder = ResNet + ASPP + Conv2d
+"""
 class Encoder(nn.Module):
     def __init__(self, input_channels, res_block, num_blocks_of_layers, num_classes):
-        self.inner_channels = 64
         self.input_channels = input_channels
         super(Encoder, self).__init__()
 
@@ -98,6 +99,7 @@ class Encoder(nn.Module):
         paddings = [6, 12, 18]
         self.aspp = ASPP(input_channels=2048, output_channels=256, depth=512, dilation_series=dilations, padding_series=paddings)
 
+        # the classifier is only for training to calculate loss
         self.main_classifier = nn.Conv2d(256, num_classes, kernel_size=1)
         self.softmax = nn.Sigmoid()#nn.Softmax()
 
@@ -107,6 +109,24 @@ class Encoder(nn.Module):
             elif isinstance(m, nn.BatchNorm2d):
                 m.weight.data.fill_(1)
                 m.bias.data.zero_()
+
+    def get_params(self, level="none" or "backbone" or "classifier"):
+        modules_with_params = []
+        if level == "none":
+            # nothing
+        elif level == "backbone":
+            modules_with_params.append(self.backbone.conv1)
+            modules_with_params.append(self.backbone.bn1)
+            modules_with_params.append(self.backbone.layer1)
+            modules_with_params.append(self.backbone.layer2)
+            modules_with_params.append(self.backbone.layer3)
+            modules_with_params.append(self.backbone.layer4)
+            modules_with_params.append(self.aspp)
+        elif level == "classifier":
+            modules_with_params.append(self.main_classifier)
+
+        return modules_with_params
+
 
     def forward(self, x):
         input_size = x.size()[2:] # H, W
@@ -122,55 +142,22 @@ class Encoder(nn.Module):
         return features, annotation
 
 
-class DepthEncoder_Convs(nn.Module):
-    def __init__(self, output_channels):
-        self.inner_channels = 16
-        self.output_channels = output_channels
-        super(DepthEncoder_Convs, self).__init__()
+"""
+DepthEncoder = ResNet + ASPP
+"""
+class DepthEncoder_ResNetASPP(nn.Module):
+    def __init__(self, output_channels, res_block, num_blocks_of_layers, num_classes):
+        super(DepthEncoder_ResNetASPP, self).__init__()
 
-        self.conv1 = nn.Conv2d(1, self.inner_channels, kernel_size=3, stride=1)
-        self.conv2 = nn.Conv2d(self.inner_channels, self.output_channels, kernel_size=3, stride=1)
-        self.bn = nn.BatchNorm2d(self.output_channels)
-        self.relu = nn.ReLU(inplace=True)
-        # self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1, ceil_mode=True)  # change ceil_mode=True
-
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                m.weight.data.normal_(0, 0.01)
-            elif isinstance(m, nn.BatchNorm2d):
-                m.weight.data.fill_(1)
-                m.bias.data.zero_()
-
-    def forward(self, x):
-        input_size = x.size()[2:]
-        features = self.conv1(x)
-
-        features = self.conv2(features)
-        features = self.bn(features)
-        features = self.relu(features)
-        # features = self.maxpool(features)
-
-        z = F.upsample(features, input_size, mode='bilinear')  #upsample to the size of input image, scale=8
-
-        return z
-
-
-class DepthEncoder_ResNet(nn.Module):
-    def __init__(self, input_channels, res_block, num_blocks_of_layers, num_classes):
-        self.inner_channels = 64
-        self.input_channels = input_channels
-        super(DepthEncoder_ResNet, self).__init__()
-
-        self.backbone = rn.ResNet(input_channels, res_block, num_blocks_of_layers, num_classes)
-
-        if False:
-            dilations = [ 6, 12, 18]
-            paddings = [6, 12, 18]
-            self.aspp = ASPP(input_channels=2048, output_channels=256, depth=512, dilation_series=dilations, padding_series=paddings)
-        else:
-            self.conv= nn.Conv2d(2048, 256, kernel_size=1,stride=1)
-            self.bn = nn.BatchNorm2d(256)
-            self.relu = nn.ReLU(inplace=True)
+        self.input_channels = 1
+        # output_channels = 256
+        self.backbone = rn.ResNet(self.input_channels, res_block, num_blocks_of_layers, num_classes)
+        dilations = [ 2, 3, 7]
+        paddings = [2, 3, 7]
+        # dilations = [ 6, 12, 18]
+        # paddings = [6, 12, 18]
+        num_channels_from_backbone = 2048
+        self.aspp = ASPP(input_channels=num_channels_from_backbone, output_channels=output_channels, depth=512, dilation_series=dilations, padding_series=paddings)
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
@@ -178,17 +165,142 @@ class DepthEncoder_ResNet(nn.Module):
             elif isinstance(m, nn.BatchNorm2d):
                 m.weight.data.fill_(1)
                 m.bias.data.zero_()
+
+
+    def get_params(self):
+        modules_with_params = []
+        modules_with_params.append(self.backbone)
+        modules_with_params.append(self.aspp)
+
+        return modules_with_params
+
 
     def forward(self, x):
         input_size = x.size()[2:] # H, W
 
         features = self.backbone(x)
-        if False:
-            features = self.aspp(features)
-        else:
-            features = self.conv(features)
-            features = self.bn(features)
-            features = self.relu(features)
+        features = self.aspp(features)
+        return features
 
+
+"""
+DepthEncoder = ResNet + Conv2d
+"""
+class DepthEncoder_ResNet(nn.Module):
+    def __init__(self, output_channels, res_block, num_blocks_of_layers, num_classes):
+        super(DepthEncoder_ResNet, self).__init__()
+        #output_channels = 256
+        self.input_channels = 1
+        self.backbone = rn.ResNet(self.input_channels, res_block, num_blocks_of_layers, num_classes)
+        num_channels_from_backbone = 2048
+        self.conv= nn.Conv2d(num_channels_from_backbone, output_channels, kernel_size=1,stride=1)
+        self.bn = nn.BatchNorm2d(output_channels)
+        self.relu = nn.ReLU(inplace=True)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                m.weight.data.normal_(0, 0.01)
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+
+    def get_params(self):
+        modules_with_params = []
+        # modules_from_backbone = self.backbone.get_params()
+        # modules_with_params.extend(modules_from_backbone)
+        modules_with_params.append(self.backbone)
+        modules_with_params.append(self.conv)
+        modules_with_params.append(self.bn)
+
+        return modules_with_params
+
+
+    def forward(self, x):
+        input_size = x.size()[2:] # H, W
+
+        features = self.backbone(x)
+        features = self.conv(features)
+        features = self.bn(features)
+        features = self.relu(features)
 
         return features
+
+
+
+# class DepthEncoderDecoder_PlainConvs(nn.Module):
+#     def __init__(self, output_channels):
+#         self.inner_channels = 32 # 16
+#         self.output_channels = output_channels
+#         super(DepthEncoderDecoder_PlainConvs, self).__init__()
+
+#         self.conv1 = nn.Conv2d(1, self.inner_channels, kernel_size=3, stride=1)
+#         self.bn1 = nn.BatchNorm2d(self.inner_channels)
+#         self.conv2 = nn.Conv2d(self.inner_channels, self.output_channels, kernel_size=3, stride=1)
+#         self.bn2 = nn.BatchNorm2d(self.output_channels)
+#         self.relu = nn.ReLU(inplace=True)
+#         # self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1, ceil_mode=True)  # change ceil_mode=True
+#         # self.conv3 = nn.Conv2d(self.output_channels, 1, kernel_size=1)
+#         # self.tan = nn.Tanh()
+
+#         for m in self.modules():
+#             if isinstance(m, nn.Conv2d):
+#                 m.weight.data.normal_(0, 0.01)
+#             elif isinstance(m, nn.BatchNorm2d):
+#                 m.weight.data.fill_(1)
+#                 m.bias.data.zero_()
+
+#     def forward(self, x):
+#         input_size = x.size()[2:]
+#         features = self.conv1(x)
+#         features = self.bn1(features)
+#         features = self.relu(features)
+
+#         features = self.conv2(features)
+#         features = self.bn2(features)
+#         features = self.relu(features)
+#         # features = self.maxpool(features)
+#         #z = F.upsample(features, input_size, mode='bilinear')  #upsample to the size of input image, scale=8
+#         return features
+
+
+# class DepthEncoder_Convs(nn.Module):
+#     def __init__(self, output_channels):
+#         self.inner_channels = 32
+#         self.output_channels = output_channels
+#         super(DepthEncoder_Convs, self).__init__()
+ 
+#         self.conv1 = nn.Conv2d(1, self.inner_channels, kernel_size=3, stride=1)
+#         self.bn1 = nn.BatchNorm2d(self.inner_channels)
+#         self.conv2 = nn.Conv2d(self.inner_channels, self.output_channels, kernel_size=3, stride=1)
+#         self.bn2 = nn.BatchNorm2d(self.output_channels)
+#         self.relu = nn.ReLU(inplace=True)
+#         self.conv3 = nn.Conv2d(self.output_channels, 1, kernel_size=1)
+#         self.tan = nn.Tanh()
+
+#          # self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1, ceil_mode=True)  # change ceil_mode=True
+ 
+#         for m in self.modules():
+#             if isinstance(m, nn.Conv2d):
+#                 m.weight.data.normal_(0, 0.01)
+#             elif isinstance(m, nn.BatchNorm2d):
+#                 m.weight.data.fill_(1)
+#                 m.bias.data.zero_()
+
+#     def forward(self, x):
+#         input_size = x.size()[2:]
+#         features = self.conv1(x)
+#         features = self.bn1(features)
+#         features = self.relu(features)
+ 
+#         features = self.conv2(features)
+#         features = self.bn2(features)
+#         features = self.relu(features)
+
+#         #features = self.conv3(features)
+#         #features = self.tan(features)
+#          # features = self.maxpool(features)
+ 
+#         features = F.upsample(features, input_size, mode='bilinear')  #upsample to the size of input image, scale=8
+#         #features = F.upsample(features, input_size, mode='bilinear')  #upsample to the size of input image, scale=8
+#         return features
